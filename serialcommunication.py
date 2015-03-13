2773b2b7f96417341f66d207998774f54b39d7f5
osama <seem.1993@gmail.com>
2015-03-13 23:15:14 +0200

    Added the code that sends a message repeatidly WITHOUT sleep and ran a thread that reads a line and print it !

---
+++ serialcommunication.py
@@ -0,0 +1,45 @@
+__author__ = 'OSAMA'
+# This file tests serial communication with Arduino UNO
+# when sending a message of data very quickly; Also,
+# examine the initialization flag.
+
+# Imports
+import serial
+import time
+import threading
+
+def main():
+    # All the main code resides here.
+    def read():
+        try:
+            while True:
+                if s.inWaiting() > 0:
+                    print s.readline().strip('\n\r')
+        except KeyboardInterrupt:
+            s.close()
+
+
+    comport = "COM5"
+    baudrate = 115200
+    # timeout = 0.1
+    # writetimeout = 0.1
+
+    s = serial.Serial(comport,baudrate)
+    readthread = threading.Thread(target=read)
+
+    while not s.isOpen():
+        s.open()
+    else:
+        print "Serial Port is open"
+        time.sleep(0.1)
+        message = "*255,0,-255,0,1$"
+        readthread.start()
+        try:
+            while True:
+                s.write(message)
+                # print "sent:", message
+        except KeyboardInterrupt:
+            s.close()
+
+if __name__ == "__main__":
+    main()

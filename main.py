e8c61a49f4324942c1fb73ad80774bbf37fd8042
osama <seem.1993@gmail.com>
2015-03-18 22:04:04 +0200

    Fixed the distortion problem in serial communication.
    - Flushes is the reason.
    - Flushes are not necessary at all and you've to turn around it if you needed to clear the serial buffer somehow.
    - Commented all flushes in this code and Arduino code and everything works just fine.
    - Simultaneous send/receive via serial port worked perfectly with almost no delay at all (need to be tested).
    - NOTICE: port names are in COMs for windows not for linux. Need to be changed to raspberry pi.

--- main.py
+++ main.py
@@ -1,20 +1,36 @@
+# In this commit
+# We set the Arduino to send 0 in order to run main() (see line 80).
+# We set the arduino code to send the message it receives.
+# The goal is to send a message and gurantee that it reaches
+# the arduino without distortion.
+# #################################################################################
+# IMPORTANT: chaos flushes causes the message to be distorted, which is logical !
+# #################################################################################
 from nodes import Arduino, Raspberry
 import threading
 import time
 def main():
     # print ("main")
+    x = 0
     rpi = Raspberry(5000,"127.0.0.2")
     def reading():
         while True:
             if ard.s.inWaiting() > 0:
+                # print "reading .. inWaiting"
                 r = ard.s.readline()
+                # print "After readline"
                 r = r.strip('\r\n')
-                print r
-                time.sleep(100)
+                print r, x  # x is the number of chars sent to Arduino.
+                            # put to check if the distorted messages are
+                            # sent distorted in the first place or not.
+                # ard.s.flushInput()
+                # time.sleep(0.1)
+
     re = threading.Thread(target=reading)
     ardmsg = []
     data = []
-    # re.start()
+    # ard.flush()
+    re.start()
     while True:
     #Receive joyStick movements from pilot
         try:
@@ -30,8 +46,8 @@ def main():
             ardmsg.append(data[3])
             ardmsg.append(data[9])
         if ard.s.isOpen():
-            ard.send(ardmsg)
-            ard.flush()
+            x = ard.send(ardmsg)    # Here is x, the number of characters sent.
+            # ard.s.flushOutput()     # flush output
         else :
             return -1
 
@@ -61,13 +77,17 @@ def main2():
             ardmsg.append(data[9])
         if ard.s.isOpen():
             ard.send(ardmsg)
+            time.sleep(0.01)
             ard.flush()
         else :
             return -1
         ardmsg = []        
 
 if __name__ == "__main__":
-    ard = Arduino("COM5")
+    ard = Arduino("COM5")   # Must be put before while True not to open the same port twice.
+    ard.s.baudrate = 9600
+    global x
+    x = 0
     while True:
         time.sleep(2)
         chk = "-1"

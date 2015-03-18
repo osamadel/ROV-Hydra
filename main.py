d001da612dbad27450f8c943d8b55e396f6ca688
Salsabeelyf <salsabeelsay@gmail.com>
2015-03-18 19:42:52 +0200

    new main

--- main.py
+++ main.py
@@ -1,38 +1,48 @@
-from nodes import Arduino , Raspberry
-import serial
-
+from nodes import Arduino, Raspberry
+import threading
+import time
 def main():
-    print ("main")
-    rpi = Raspberry(5000,"192.168.1.10")
+    # print ("main")
+    rpi = Raspberry(5000)
+    def reading():
+        while True:
+            if ard.s.inWaiting():
+                r = ard.s.readline()
+                r = r.strip('\r\n')
+                print r
+                time.sleep(100)
+    re = threading.Thread(target= reading)
     ardmsg = []
     data = []
+    re.start()
     while True:
     #Receive joyStick movements from pilot
         try:
             data = rpi.receive()
-            print (data)
         except Exception as e:
             print ("Nothing to receive")
     #Save the msg
-        if (data[0] == "PILOT"):
+        if data[0] == "PILOT":
             ardmsg.append(data[1])
             ardmsg.append(data[2])
             ardmsg.append(data[4])
             ardmsg.append(data[3])
             ardmsg.append(data[9])
-        if (ard.s.isOpen()):
+        if ard.s.isOpen():
             ard.send(ardmsg)
             ard.flush()
         else :
             return -1
+
+
         ardmsg = []        
-            
+
 
 
 
 def main2():
     print ("main2")
-    rpi = Raspberry(5001,"192.168.1.10")
+    rpi = Raspberry(5001)
     ardmsg = []
     data = []
     while True:
@@ -42,13 +52,13 @@ def main2():
         except Exception as e:
             print ("Nothing to receive")
     #Save the msg
-        if (data[0] == "PILOT"):
+        if data[0] == "PILOT":
             ardmsg.append(data[1])
             ardmsg.append(data[2])
             ardmsg.append(data[4])
             ardmsg.append(data[6])
             ardmsg.append(data[9])
-        if (ard.s.isOpen()):
+        if ard.s.isOpen():
             ard.send(ardmsg)
             ard.flush()
         else :
@@ -57,17 +67,17 @@ def main2():
 
 if __name__ == "__main__":
     while True:
-        ard = Arduino("/dev/ttyACM0")
+        ard = Arduino("COM")
         chk = "-1"
-        if ( not ard.s.isOpen()):
+        if not ard.s.isOpen():
             ard.s.open()
-        chk = ard.s.write("255")
+        chk = ard.s.write("2")
         chk = ard.s.readline()
         chk = chk.strip('\n\r')
         rtrn = 0
-        if(chk == "0"):
+        if chk == "0":
             rtrn = main()
-        elif(chk == "1"):
+        elif chk == "1":
             rtrn = main2()
         if(rtrn == -1):
             continue

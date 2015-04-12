c896e6acb7815f6f4608fd95f8da3331443f5091
osama <seem.1993@gmail.com>
2015-04-12 18:31:44 +0200

    Made the code more modular and flexible.

--- main.py
+++ main.py
@@ -9,105 +9,80 @@
 from nodes import Arduino, Raspberry
 import threading
 import time
-def main():
-    # print ("main")
-    x = 0
-    rpi = Raspberry(5000,"192.168.1.6")
-    def reading():
+
+
+def reading():
         while True:
-            if ard.s.inWaiting() > 0:
-                # print "reading .. inWaiting"
-                r = ard.s.readline()
-                # print "After readline"
-                r = r.strip('\r\n')
-                print r, x  # x is the number of chars sent to Arduino.
+            if ard.inWaiting() > 0:
+                r = ard.get()
+                print r     # x is the number of chars sent to Arduino.
                             # put to check if the distorted messages are
                             # sent distorted in the first place or not.
-                # ard.s.flushInput()
-                # time.sleep(0.1)
 
+def makemessage(data,seq,ardmsg):
+    if data[0] == "PILOT":
+            for i in seq:
+                ardmsg.append(data[i])
+
+def main():
+    rpi = Raspberry(5000,"127.0.0.2")
     re = threading.Thread(target=reading)
     ardmsg = []
     data = []
-    # ard.flush()
+    msgseq = [1,    # Xspeed
+              2,    # Rspeed
+              4,    # Zspeed
+              3,    # Yspeed
+              9]    # Turbo
     re.start()
     while True:
-    #Receive joyStick movements from pilot
+        #Receive joyStick movements from pilot
         try:
             data = rpi.receive()
         except Exception as e:
-            print ("Nothing to receive")
-        # print data
-    #Save the msg
-        if data[0] == "PILOT":
-            ardmsg.append(data[1])
-            ardmsg.append(data[2])
-            ardmsg.append(data[4])
-            ardmsg.append(data[3])
-            ardmsg.append(data[9])
-        if ard.s.isOpen():
-            x = ard.send(ardmsg)    # Here is x, the number of characters sent.
-            # ard.s.flushOutput()     # flush output
+            print "Error receiving from PilotConsole"
+        makemessage(data,msgseq,ardmsg)
+        if ard.isOpen():
+            x = ard.putList(ardmsg)    # Here is x, the number of characters sent.
         else :
             return -1
-
-
-        ardmsg = []        
-
-
+        ardmsg = []
 
 
 def main2():
-    print ("main2")
     rpi = Raspberry(5001,"192.168.1.6")
+    # re = threading.Thread(target=reading)
     ardmsg = []
     data = []
+    msgseq = [1,    # Xspeed
+              2,    # Rspeed
+              4,    # Zspeed
+              6,    # Arm
+              9]    # Turbo
+    # re.start()
     while True:
-    #Receive joyStick movements from pilot
+        #Receive joyStick movements from pilot
         try:
             data = rpi.receive()
         except Exception as e:
-            print ("Nothing to receive")
-    #Save the msg
-        if data[0] == "PILOT":
-            ardmsg.append(data[1])
-            ardmsg.append(data[2])
-            ardmsg.append(data[4])
-            ardmsg.append(data[6])
-            ardmsg.append(data[9])
-        if ard.s.isOpen():
-            ard.send(ardmsg)
-            time.sleep(0.01)
-            ard.flush()
+            print "Error receiving from PilotConsole"
+        makemessage(data,msgseq,ardmsg)
+        if ard.isOpen():
+            x = ard.putList(ardmsg)    # Here is x, the number of characters sent.
         else :
             return -1
-        ardmsg = []        
+        ardmsg = []
 
 if __name__ == "__main__":
-    ard = Arduino("/dev/ttyACM0")   # Must be put before while True not to open the same port twice.
-    # ard.s.baudrate = 112
-    global x
-    x = 0
+    ard = Arduino("COM5")   # Must be put before while True not to open the same port twice.
+    time.sleep(2)	# This line solved a problem .. it is important.
     while True:
-        time.sleep(2)	# This line solved a problem .. it is important.
-        chk = "-1"
-        if not ard.s.isOpen():
-            ard.s.open()
-        else:
-#            chk = ard.s.write("255")
-            ard.s.write('2')
-            print chk
-        print "Written"
-        if ard.s.inWaiting() > 0:
-            chk = ard.s.readline()
-            print "Read"
-            chk = chk.strip('\n\r')
-            rtrn = 0
-            if chk == "2":
-                rtrn = main()
-            elif chk == "1":
-                rtrn = main2()
-            if(rtrn == -1):
-                continue
-        
-            
+        chk = ard.initConn()
+        print "CHECK =", chk
+        rtrn = 0
+        if chk == "2":
+            rtrn = main()
+        elif chk == "1":
+            rtrn = main2()
+        if(rtrn == -1):
+            continue
